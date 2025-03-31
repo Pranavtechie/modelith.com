9cbda085b26d8de316a7a41631e3b6220e567f87
Pranav <pranav.techiegeek@gmail.com>
2025-03-31 23:01:46 +0530

    fix: add pypi link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
 export default function Header() {
 	return (
 		<header className="h-12 p-2 flex gap-2 bg-gray-700 text-gray-100 justify-between">
-			<nav className="flex flex-row items-center">
-				<div className="px-2 font-bold hover:underline">
+			<nav className="flex flex-row items-center gap-4">
+				<div>
 					<a
-						href="https://github.com/Pranavtechie/modelith.com"
+						className="px-2 font-bold hover:underline"
+						href="https://github.com/Pranavtechie/modelith-cli"
 						target="_blank"
 					>
 						GitHub
+					</a>
+
+					<a
+						className="px-2 font-bold hover:underline"
+						href="https://pypi.org/project/modelith/"
+						target="_blank"
+					>
+						PyPi
 					</a>
 				</div>
 			</nav>
