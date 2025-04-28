abfd00ae7c2abd1ec9bc6a1e0c3f9196f948496c
Pranav <pranav.techiegeek@gmail.com>
2025-04-28 13:23:11 +0530

    fix: change to npm from PyPI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,25 @@
 export default function Header() {
-	return (
-		<header className="h-12 p-2 flex gap-2 bg-gray-700 text-gray-100 justify-between">
-			<nav className="flex flex-row items-center gap-4">
-				<div>
-					<a
-						className="px-2 font-bold hover:underline"
-						href="https://github.com/Pranavtechie/modelith-cli"
-						target="_blank"
-					>
-						GitHub
-					</a>
+  return (
+    <header className="h-12 p-2 flex gap-2 bg-gray-700 text-gray-100 justify-between">
+      <nav className="flex flex-row items-center gap-4">
+        <div>
+          <a
+            className="px-2 font-bold hover:underline"
+            href="https://github.com/Pranavtechie/modelith-cli"
+            target="_blank"
+          >
+            GitHub
+          </a>
 
-					<a
-						className="px-2 font-bold hover:underline"
-						href="https://pypi.org/project/modelith/"
-						target="_blank"
-					>
-						PyPI
-					</a>
-				</div>
-			</nav>
-		</header>
-	);
+          <a
+            className="px-2 font-bold hover:underline"
+            href="https://pypi.org/project/modelith/"
+            target="_blank"
+          >
+            npm
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
